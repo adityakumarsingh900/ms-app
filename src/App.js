5121c7e57320249c4e171c07e058ee4b580ab995
Aditya <adityakumarsingh@gmail.com>
2023-07-21 12:31:59 +0530

    big bang

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [msalClient, setMsalClient] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+
+  function login(e) {
+    e.preventDefault();
+    const scopes = ['Calendars.Read'];
+    msalClient.loginRedirect({
+      scopes
+    });
+  }
+
+  function logout(e) {
+    e.preventDefault();
+    msalClient.logoutRedirect();
+  }
+
+  console.log('accounts', accounts);
+
+  useEffect(() => {
+    const { msal } = window;
+    const msalConfig = {
+      auth: {
+        clientId: '341c99e0-37f1-44e8-a93e-39c66742a1a6',
+        authority: 'https://login.microsoftonline.com/consumers/'
+      }
+    };
+
+    const msalClient = new msal.PublicClientApplication(msalConfig);
+    setMsalClient(msalClient);
+
+    msalClient
+        .handleRedirectPromise()
+        .then(response => {
+          const accounts = msalClient.getAllAccounts();
+
+          setAccounts(accounts);
+        });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {accounts.length === 0 ? (
+        <button onClick={login}>Login</button>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
     </div>
   );
 }
