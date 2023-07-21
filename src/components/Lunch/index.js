c5ca468eb9f05e6f2efb7b2b1ed8ddc4099c7965
Ashish kumar <ashishkumar@Ashishs-MacBook-Air.local>
2023-07-21 17:01:06 +0530

    array of objects

--- src/components/Lunch/index.js
+++ src/components/Lunch/index.js
@@ -15,17 +15,25 @@ function Lunch() {
             // configure Microsoft Graph query to retrieve upcoming meetings for today
             const now = new Date();
             const midnight = new Date();
-            midnight.setDate(midnight.getDate() + 1);
-            midnight.setHours(0);
+            now.setHours(9);
+            now.setMinutes(0);
+            now.setSeconds(0);
+            now.setMilliseconds(0);
+            midnight.setHours(19);
             midnight.setMinutes(0);
             midnight.setSeconds(0);
             midnight.setMilliseconds(0);
-    
             graphClient
               .api(`/me/calendarview?startdatetime=${now.toISOString()}&enddatetime=${midnight.toISOString()}&$orderby=start/dateTime`)
               .get()
               .then(res => {
                 console.log('todays meetings', res.value);
+                let arrStartTime = [{start: 9, end: 9}];
+                res.value?.map((data)=>{
+                    arrStartTime.push({start: data.start.dateTime, end:data.end.dateTime});
+                })
+                arrStartTime.push({start: 19, end: 19});
+                console.log('test6', arrStartTime);
                 if (res.value.length === 0) {
                 }
                 else {
